fd0adcb585541b5ae4702f5645d33f255461e8f2
goorockey <kelvingu616@gmail.com>
2013-05-01 14:51:00 +0800

    index body

--- hk/models.py
+++ hk/models.py
@@ -1,36 +1,53 @@
 from django.db import models
-import datetime
+import datetime, urlparse
+from django.utils import timezone
 
 class User(models.Model):
     username = models.CharField(max_length = 50, unique = True)
     password = models.CharField(max_length = 50)
     about = models.TextField(blank = True)
-    email = models.CharField(max_length = 50)
+    email = models.EmailField()
     register_date = models.DateTimeField(default = datetime.datetime.now())
+
 
 class New(models.Model):
     title = models.CharField(max_length = 100)
-    url = models.TextField(null = True)
-    text = models.TextField(null = True)
-    author_id = models.ForeignKey(User)
+    url = models.URLField(blank = True)
+    text = models.TextField(blank = True)
+    author = models.ForeignKey(User)
     create_date = models.DateTimeField(default = datetime.datetime.now())
     points = models.IntegerField(default = 0)
     scores = models.IntegerField(default = 0)
     comments = models.IntegerField(default = 0)
 
+    def formated_create_time(self):
+        now = timezone.now() # offset-awared datetime
+        now.astimezone(timezone.utc).replace(tzinfo=None)
+        delta = now - self.create_date
+        if delta.days > 0:
+            return "%s day(s) ago" % (delta.days)
+        else:
+            if (delta.seconds / 3600) > 0:
+                return "%s hour(s) ago" % (delta.seconds / 3600)
+            else:
+                return "%s minute(s) ago" % (delta.seconds / 60)
+
+    def get_domain(self):
+        return urlparse.urlsplit(self.url).netloc
+
 class Comment(models.Model):
-    author_id = models.ForeignKey(User)
-    parent_id = models.ForeignKey('self')
+    author = models.ForeignKey(User)
+    parent = models.ForeignKey('self')
     create_date = models.DateTimeField(default = datetime.datetime.now())
     points = models.IntegerField(default = 0)
     scores = models.IntegerField(default = 0)
     comments = models.IntegerField(default = 0)
 
 class New_Points(models.Model):
-    user_id = models.ForeignKey(User)
-    new_id = models.ForeignKey(New)
+    user = models.ForeignKey(User)
+    new = models.ForeignKey(New)
 
 class Comment_Points(models.Model):
-    user_id = models.ForeignKey(User)
+    user = models.ForeignKey(User)
     comment_id = models.ForeignKey(Comment)
     
--- hk/views.py
+++ hk/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from hk.models import *
+
+page_list = {'new' : '/', 'comments' : '/comments', 'submit': '/submit'}
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', 
+            {'page_list': page_list,
+             'new_list' : New.objects.all()})
